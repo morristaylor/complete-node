8765363b32ced8a6d6c63ae1a2c985c1892f712f
Taylor Morris <taylor.morris@gmail.com>
2018-10-24 16:08:20 -0700

    more promise work, new playground file, wrapped geocodeAddress request in promise, duplicated .env to get GMaps key working

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -11,20 +11,23 @@ var asyncAdd = (a, b) => {
 }
 
 asyncAdd(1, 2).then((res) => {
-  console.log('Result:', res);
-}, (errorMessage) => {
+  console.log('First result:', res);
+  return asyncAdd(res, 10);
+}).then((res) => {
+  console.log('Next result should be 13 >>>', res);
+}).catch((errorMessage) => {
   console.log(errorMessage);
 });
 
-var somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Hey. It worked!');
-    reject('Unable to fulfill promise');
-  }, 2500)
-});
-
-somePromise.then((message) => {
-  console.log('Success:', message);
-}, (errorMessage) => {
-  console.log('Error:', errorMessage);
-});
+// var somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Hey. It worked!');
+//     reject('Unable to fulfill promise');
+//   }, 2500)
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success:', message);
+// }, (errorMessage) => {
+//   console.log('Error:', errorMessage);
+// });
