55ea70a81ae30cb258c3a899fa06765d7beb5c60
Taylor Morris <taylor.morris@gmail.com>
2018-10-23 22:10:37 -0700

    cleaning up playground files

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -16,15 +16,15 @@ asyncAdd(1, 2).then((res) => {
   console.log(errorMessage);
 });
 
-// var somePromise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('Hey. It worked!');
-//     reject('Unable to fulfill promise');
-//   }, 2500)
-// });
-//
-// somePromise.then((message) => {
-//   console.log('Success:', message);
-// }, (errorMessage) => {
-//   console.log('Error:', errorMessage);
-// });
+var somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hey. It worked!');
+    reject('Unable to fulfill promise');
+  }, 2500)
+});
+
+somePromise.then((message) => {
+  console.log('Success:', message);
+}, (errorMessage) => {
+  console.log('Error:', errorMessage);
+});
