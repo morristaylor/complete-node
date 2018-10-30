4aa579c686f49f3f01786136506f5097c91bda76
Taylor Morris <taylor.morris@gmail.com>
2018-10-29 22:50:56 -0700

    added Mocha, built a few simple tests in utils

---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,15 @@
+const utils = require('./utils.js');
+
+it('should all two numbers', () => {
+  var res = utils.add(33, 11);
+  if (res !== 44) {
+    throw new Error(`Expected 44 but got ${res}.`);
+  }
+});
+
+it('should square a number', () => {
+  var res = utils.square(3);
+  if (res !== 9) {
+    throw new Error(`Expected 9 but got ${res}.`);
+  }
+});
