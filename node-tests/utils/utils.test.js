efe8fa737891f3b5ca3b595333d0aad908ba7172
Taylor Morris <taylor.morris@gmail.com>
2018-10-30 21:13:39 -0700

    corrected typo

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,6 +1,6 @@
 const utils = require('./utils.js');
 
-it('should all two numbers', () => {
+it('should add two numbers', () => {
   var res = utils.add(33, 11);
   if (res !== 44) {
     throw new Error(`Expected 44 but got ${res}.`);
@@ -13,3 +13,10 @@ it('should square a number', () => {
     throw new Error(`Expected 9 but got ${res}.`);
   }
 });
+
+// it('should square a number', () => {
+//   var res = utils.square(3);
+//   if (res !== 9) {
+//     throw new Error(`Expected 9 but got ${res}.`);
+//   }
+// });
