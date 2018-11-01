a14325409b7f8b66c266b0b529aac40b5f1940f1
Taylor Morris <taylor.morris@gmail.com>
2018-10-31 22:58:51 -0700

    added expect assertion library and rewrote a few tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,25 @@
+const expect = require('expect');
+
 const utils = require('./utils.js');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44 but got ${res}.`);
-  }
+
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9 but got ${res}.`);
-  }
+
+  expect(res).toBe(9).toBeA('number');
+});
+
+it('should set firstName and lastName', () => {
+  var user = {location: 'Portland', age: 25};
+  var res = utils.setName(user, 'John Smith');
+
+  expect(res).toInclude({
+    firstName: 'John',
+    lastName: 'Smith'
+  });
 });
