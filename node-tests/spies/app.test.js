e53a7989a3c975975697c46405d73c4e5127c0d9
Taylor Morris <taylor.morris@gmail.com>
2018-11-04 09:19:16 -0800

    final commit for section 6, added rewire and sample tests using spies

---
+++ node-tests/spies/app.test.js
@@ -0,0 +1,26 @@
+const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
+
+describe('App', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
+
+  it('should call the spy correctly', () => {
+    var spy = expect.createSpy();
+    spy('John', 40);
+    expect(spy).toHaveBeenCalledWith('John', 40);
+  });
+
+  it('should call saveUser with user object', () => {
+    var email = 'andrew@example.com';
+    var password = '123abc';
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
+  });
+
+});
